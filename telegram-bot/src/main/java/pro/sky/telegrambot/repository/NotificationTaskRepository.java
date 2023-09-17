5c7603449349505e29434a9d9ce9a869e6bbb983
hp <747685@mail.ru>
2023-09-17 15:34:35 +0300

    telegram bot

---
+++ telegram-bot/src/main/java/pro/sky/telegrambot/repository/NotificationTaskRepository.java
@@ -0,0 +1,15 @@
+package pro.sky.telegrambot.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.RequestMapping;
+import pro.sky.telegrambot.model.NotificationTask;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Repository
+public interface NotificationTaskRepository extends JpaRepository<NotificationTask, Long> {
+
+    List<NotificationTask> findAllByNotificationDateTime(LocalDateTime notificationDateTime);
+}
